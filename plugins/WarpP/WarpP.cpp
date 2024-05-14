3a0dc3d819b9c521c1cc49e8d08bf1d1ecd6d538
Aleksandar <36089376+aleksandarkoruga@users.noreply.github.com>
2024-05-15 00:51:02 +0300

    Update WarpP.cpp
    Insert original comment from the SuperCollider source code

--- plugins/WarpP/WarpP.cpp
+++ plugins/WarpP/WarpP.cpp
@@ -1,6 +1,35 @@
 // PluginWarpP.cpp
 // Aleksandar Koruga (aleksandar.koruga@gmail.com)
 
+//Based on the original:
+/*
+ *  GrainUGens.cpp
+ *  xSC3plugins
+ *
+ *  Created by Josh Parmenter on 2/4/05.
+ *  Copyright 2005 __MyCompanyName__. All rights reserved.
+ *
+ */
+/*
+    SuperCollider real time audio synthesis system
+    Copyright (c) 2002 James McCartney. All rights reserved.
+    http://www.audiosynth.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
 #include "SC_PlugIn.hpp"
 #include "WarpP.hpp"
 
